97d51a13ea382ea445b878d10d7632a92830b9b1
Khushboo Verma <khush.vv@gmail.com>
2020-01-11 00:03:58 +0530

    Added jenja templating

--- flaskblog.py
+++ flaskblog.py
@@ -25,7 +25,7 @@ def home():
 @app.route("/about")
 def about():
     #return "<h1>Welcome to About Page!</h1>"
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
 
 if __name__ == "__main__":
     app.run(debug=True)
