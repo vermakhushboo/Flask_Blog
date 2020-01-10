5b6ce0e1fecd59b2012c876935d65f7d3ad40773
Khushboo Verma <khush.vv@gmail.com>
2020-01-10 23:48:43 +0530

    Added Home and About pages

---
+++ flaskblog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Khushboo Verma',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_created': 'April 22, 2019'
+    },
+    {
+        'author': 'Kartik Verma',
+        'title': 'Blog Post Copy',
+        'content': 'I copied didis post',
+        'date_created': 'May 25, 2019'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    #return "<h1>Welcome to Home Page!</h1>"
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    #return "<h1>Welcome to About Page!</h1>"
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
